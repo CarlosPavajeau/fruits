7bf406f9717a71f31e54c1ab70d60a5788913730
Carlos Pavajeau <cantte098@gmail.com>
2022-07-05 15:21:05 -0500

    fix: add @ReflectiveAccess

--- src/main/kotlin/example/micronaut/Fruit.kt
+++ src/main/kotlin/example/micronaut/Fruit.kt
@@ -2,11 +2,13 @@ package example.micronaut
 
 import io.micronaut.core.annotation.Creator
 import io.micronaut.core.annotation.Introspected
+import io.micronaut.core.annotation.ReflectiveAccess
 import org.bson.codecs.pojo.annotations.BsonCreator
 import org.bson.codecs.pojo.annotations.BsonProperty
 import javax.validation.constraints.NotBlank
 
 @Introspected
+@ReflectiveAccess
 data class Fruit @Creator @BsonCreator constructor(
     @field:BsonProperty("name") @param:BsonProperty("name") @field:NotBlank val name: String,
     @field:BsonProperty("description") @param:BsonProperty("description") var description: String?
